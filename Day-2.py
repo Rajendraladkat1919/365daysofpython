d27fe23446f9214d93358334b0b7e6bf7979e9d0
rajendraladkat <rajendraladkat1910@gmail.com>
2024-01-03 09:46:43 +0530

    Day1 and Day2 code submisison.

---
+++ Day-2.py
@@ -0,0 +1,24 @@
+#basic input and output.
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    
+    return print("Hello ", input())
+    
+
+if __name__ == '__main__':
+    main()
+'''
+
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    x=input()
+    print("Hello",x)
+if __name__ == '__main__':
+    main()
+
+
+'''    
